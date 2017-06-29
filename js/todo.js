c05e7dd99c8f650c5a8241a376bcbf73ae845ee0
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2017-06-29 16:58:22 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   // call functions here
+  submitForm();
 });
 
+function submitForm(){
+  $('form').on('submit', function(event){
+    let item = $('#item').val();
+    $('ol').append( '<li>' + item + '</li>' );
+    event.preventDefault();
+  
+  });
+}
 // define functions here
